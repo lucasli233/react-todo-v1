822cb1920bdc216daf1911642f25be0bc3d210a4
Oblivion233 <lucaaas2333@outlook.com>
2020-12-15 03:39:57 +1300

    created baisc functions

---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { formatDiagnostic } from "typescript";
+//rfce
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      />
+      <button className="todo-button">Add todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
